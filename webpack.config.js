e8f3caeeb3d289f101ed139ec73cf2afcb48bd0e
rnvarma <rohan@prompthire.com>
2016-06-06 16:31:19 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require("path")
+var webpack = require('webpack')
+var BundleTracker = require('webpack-bundle-tracker')
+var pathtoassets = require('./pathtoassets')
+
+module.exports = {
+  context: __dirname,
+
+  entry: pathtoassets + '/js/index', // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
+
+  output: {
+      path: path.resolve(pathtoassets + '/bundles/'),
+      filename: "[name]-[hash].js",
+  },
+
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'}),
+  ],
+
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets:['react'] }}, // to transform JSX into JS
+      { test: /\.css$/, loaders: ["style", "css", "postcss"]},
+      { test: /\.scss$/, loaders: ["style", "css", "postcss", "sass"]},
+      { test: /\.(jsx|js)$/, loader: 'imports?jQuery=jquery,$=jquery,this=>window'},
+      { test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url?limit=10000"},
+      { test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/, loader: 'file'},
+    ],
+  },
+
+  resolve: {
+    modulesDirectories: ['node_modules', 'bower_components', 'assets'],
+    extensions: ['', '.js', '.jsx', '.css', '.scss']
+  },
+}
