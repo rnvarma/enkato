5796bef7533871e2524ada64de687820dc2cd9b9
rnvarma <rohan@prompthire.com>
2016-06-06 17:24:52 -0400

    fixed front page

--- assets/js/index.jsx
+++ assets/js/index.jsx
@@ -135,7 +135,7 @@ var CommentBox = React.createClass({
     render: function() {
         return (
             <div className="commentBox">
-                <h1>Comments!</h1>
+                <h1>Comments woooooo!</h1>
                 <CommentList data={this.state.data}/>
                 <CommentForm onCommentSubmit={this.onCommentSubmit}/>
             </div>
