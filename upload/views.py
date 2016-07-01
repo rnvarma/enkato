80f7e6cee21914f11077377f203e2d2b9726a803
Jared Moore <jmoore1@andrew.cmu.edu>
2016-07-01 18:04:23 -0400

    Moved series modal to its own class, fixed series indexing, added error output for series url entry

--- upload/views.py
+++ upload/views.py
@@ -3,33 +3,51 @@ from django.views.generic.base import View
 from django.http import HttpResponseRedirect
 from django.http import JsonResponse
 
-from backend.utility import *
+from backend.utility import getYTIdFromURL, getYTMetaData, getStringsFromStringList
+from backend.models import Tag, Video, VideoTag, Series, SeriesVideo
 
-def uploadYTVideo(url, cu):
+def uploadYTVideo(url, creator):
+    """
+    Saves video and each individual tag from the url or
+    returns None if bad url or failed API response
+    """
+
     vid_id = getYTIdFromURL(url)
-    # name, description, thumbnail, duration, tags
+    if vid_id is None: return None
+
+    # metadata contains name, description, thumbnail, duration, tags
     metadata = getYTMetaData(vid_id)
+    if metadata is None: return None
+
     tags = metadata["tags"]
 
     tag_objs = []
-    # make tag_objs
-    for tag in tags:
-        tag_objs.append(Tag.objects.get_or_create(name=tag)[0])
+    # find or create a tag so we can link it to this video
+    for tag_name in tags:
+        tag, _ = Tag.objects.get_or_create(name=tag_name)
+        tag_objs.append(tag)
 
-    video = Video(
+    # TODO: see if YouTube video blocks embedding
+
+    video = Video.objects.create(
         name=metadata["name"],
         source="youtube",
         vid_id=vid_id,
         description=metadata["description"],
         thumbnail=metadata["thumbnail"],
         duration=metadata["duration"],
-        creator=cu
+        creator=creator
     )
     video.save()
 
+    # TODO: flag if duplicate video, add option to soft link or even
+    # hard link to the original video
+
+    # store or update tags, linking to this video
     for tag_obj in tag_objs:
         video_tag = VideoTag(video=video, tag=tag_obj)
         video_tag.save()
+
     return video
 
 class UploadVideo(View):
@@ -44,20 +62,35 @@ class UploadVideo(View):
             video = uploadYTVideo(url, creator)
         return JsonResponse({'v_uuid': video.uuid})
 
-class UploadVideoToSeries(View):
-    def post(self, request, s_id):
-        urls = request.POST.get('urls')
-        series = Series.objects.get(uuid=s_id)
-        creator = request.user.customuser
-        url_list = getURLSFromStringList(urls)
-        videos = []
-        for url in url_list:
-            videos.append(uploadYTVideo(url, creator))
+# TODO: support uploading playlists, possibly by writing a function that takes
+# in a playlist, contacts the API and gets a list of videos and the just does our
+# normal thing with a list of videos
 
+class UploadVideoToSeries(View):
+    """ Takes video url list 'urls' and adds it to the series """
+
+    def post(self, request, s_id):
+        creator = request.user.customuser
+        urls = request.POST.get('urls')
+        url_list = getStringsFromStringList(urls)
+
+        # build list of newly added videos
+        videos = []
+        bad_urls = []
+        for url in url_list:
+            video = uploadYTVideo(url, creator)
+            if video is None:
+                bad_urls.append(url)
+            else:
+                videos.append(video)
+
+        series = Series.objects.get(uuid=s_id)
+        # find current video order
         curr_order_num = 0
         if series.videos.all().count() > 0:
             curr_order_num = series.videos.all().order_by("-order")[0].order + 1
 
+        # add each video to series
         for (i, video) in enumerate(videos):
             series_video = SeriesVideo(
                 series=series,
@@ -65,7 +98,11 @@ class UploadVideoToSeries(View):
                 order=i + curr_order_num
             )
             series_video.save()
-        return JsonResponse({'status': True})
+
+        if not bad_urls:
+            return JsonResponse({ 'status': True })
+        else:
+            return JsonResponse({ 'status': False, 'errors': bad_urls })
 
 
 
