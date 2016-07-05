b88d64678d7cdc07ea66f08bce1e43d6b19f760c
rnvarma <rohan@prompthire.com>
2016-07-05 17:05:34 -0400

    fixed margin bug and also saving topic edits bug

--- assets/js/series/seriespage/UploadAnnotateModal.jsx
+++ assets/js/series/seriespage/UploadAnnotateModal.jsx
@@ -92,8 +92,7 @@ module.exports = React.createClass({
                 class: "annotating",
                 body: <AnnotateVideosForSeries
                           data={this.props.data}
-                          quizMode={this.props.quizMode}
-                      />
+                          quizMode={this.props.quizMode}/>
             }
             nextText = "Finish";
         } else {
@@ -102,8 +101,7 @@ module.exports = React.createClass({
                 class: "",
                 body: <AddVideoToSeriesForm
                           urls={this.props.urls}
-                          onURLAdded={this.props.onURLImport}
-                      />
+                          onURLAdded={this.props.onURLImport}/>
             }
             nextText = "Next";
         }
