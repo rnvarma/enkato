c89d8acbd6d601b2bd5e332cd38955028216b51c
Jared Moore <jmoore1@andrew.cmu.edu>
2016-07-06 20:10:30 -0400

    Added getCookie to utility.js

--- assets/js/globals/utility.js
+++ assets/js/globals/utility.js
@@ -1,3 +1,19 @@
+/* can be used to get csrf token when name is csrftoken */
+export function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        let cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = $.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
 
 /* seconds to M:SS or H:MM:SS, decimals retained */
 export function styleDuration(seconds) {
