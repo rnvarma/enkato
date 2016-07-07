8237a5f3bb4ceb1cefedc152d05ae9285b74a21f
Jared Moore <jmoore1@andrew.cmu.edu>
2016-07-07 11:02:23 -0400

    Updated command to make new react components

--- structabl/management/commands/newreactcomponent.py
+++ structabl/management/commands/newreactcomponent.py
@@ -1,35 +1,38 @@
 from django.core.management.base import BaseCommand, CommandError
 import os
 
-baseSCSS = """
+baseSCSS = """\
 @import '../../globals/colors.scss';
 @import '../../globals/mixins.scss';
 
-.%s {
+.{camelCaseName} {
     
 }
 """
 
-baseJSX = """
-require("css/%s/%s/%s.scss");
+baseJSX = """\
+require('css/{app}/{endpoint}/{name}.scss');
 
-var React = require('react')
+import React from 'react';
 
-module.exports = React.createClass({
-    render: function() {
-        return (
-            <div className="%s">
-                %s
-            </div>
-        )
+class {name} extends React.Component {
+    constructor(props) {
+        super(props);
     }
-})
+
+    render() {
+        return (
+            <div className="{camelCaseName}">
+            </div>
+        );
+    }
+}
 """
 
 importCommand = "var %s = require('js/%s/%s/%s');"
 
 class Command(BaseCommand):
-    help = "create a new react component -- initializes jsx + scss"
+    help = 'create a new react component -- initializes jsx + scss'
 
     def add_arguments(self, parser):
         parser.add_argument('name', type=str)
@@ -37,18 +40,19 @@ class Command(BaseCommand):
         parser.add_argument('endpoint', type=str)
 
     def handle(self, *args, **options):
-        spec_path = options["app"] + os.sep + options["endpoint"] + os.sep + options["name"]
-        camelCaseName = options["name"][0].lower() + options["name"][1:]
+        spec_path = options['app'] + os.sep + options['endpoint'] + os.sep + options['name']
+        camelCaseName = options['name'][0].lower() + options['name'][1:]
         
-        scss_path = "assets/css" + os.sep + spec_path + ".scss"
-        scss_file = open(scss_path, "w+")
-        scss_file.write(baseSCSS % camelCaseName)
+        scss_path = 'assets/css' + os.sep + spec_path + '.scss'
+        scss_file = open(scss_path, 'w+')
+        scss_file.write(baseSCSS.format(camelCaseName))
         scss_file.close()
 
-        jsx_path = "assets/js" + os.sep + spec_path + ".jsx"
-        jsx_file = open(jsx_path, "w+")
-        jsx_file.write(baseJSX % (options["app"], options["endpoint"], options["name"], camelCaseName, options["name"]))
+        jsx_path = 'assets/js' + os.sep + spec_path + '.jsx'
+        jsx_file = open(jsx_path, 'w+')
+        jsx_file.write(baseJSX.format(camelCaseName=camelCaseName, **options))
         jsx_file.close()
+
         print importCommand % (options["name"], options["app"], options["endpoint"], options["name"])
         
 
