84ddaf8696867d0cc98a30ceffbe2ede852b2efb
rnvarma <rohan@prompthire.com>
2016-07-08 15:28:49 -0400

    woooo

--- assets/js/globals/DjangoImageLinkHandler.js
+++ assets/js/globals/DjangoImageLinkHandler.js
@@ -1,10 +1,5 @@
 
 module.exports = function(imgName) {
     var production = true;
-
-    if (production) {
-        return "https://enkato-static-files.s3.amazonaws.com/static/imgs/" + imgName
-    } else {
-        return "/static/imgs/" + imgName;
-    }
+    return "https://enkato-static-files.s3.amazonaws.com/static/imgs/" + imgName
 }
