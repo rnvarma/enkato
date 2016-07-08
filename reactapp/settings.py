b98bec5704790624b40420cb86b44a97f672dd73
rnvarma <rohan@prompthire.com>
2016-07-08 14:37:45 -0400

    debug settings

--- reactapp/settings.py
+++ reactapp/settings.py
@@ -142,7 +142,7 @@ MEDIAFILES_LOCATION = 'media'
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 DEFAULT_FILE_STORAGE = 'reactapp.storage.MediaStorage'
 
-if not DEBUG:
+if DEBUG:
     STATIC_URL = '/static/'
 
     STATICFILES_DIRS = (
