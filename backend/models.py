bc423de30af3053562956146a9f18a0d6308f5da
rnvarma <rohan@prompthire.com>
2016-07-14 13:58:53 -0400

    added analytics app, migrations

--- backend/models.py
+++ backend/models.py
@@ -366,15 +366,16 @@ class StudentSeriesData(models.Model):
     series = models.ForeignKey(Series, related_name="students_data")
 
 class StudentSeriesVideoData(models.Model):
-    sp_data = models.ForeignKey(StudentSeriesData, related_name="videos_data")
+    ss_data = models.ForeignKey(StudentSeriesData, related_name="videos_data")
     video = models.ForeignKey(Video, related_name="series_students_data")
     num_views = models.IntegerField(default=0)
     avg_duration_watched = models.IntegerField(default=0)  # number seconds
     seconds_into_video = models.IntegerField(default=0)
+    watched = models.BooleanField(default=False)
     completed = models.BooleanField(default=False)
 
 class StudentSeriesVideoQuizQuestionData(models.Model):
-    scv_data = models.ForeignKey(StudentSeriesVideoData, related_name="quizzes_data")
+    ssv_data = models.ForeignKey(StudentSeriesVideoData, related_name="quizzes_data")
     quiz_question = models.ForeignKey(QuizQuestion, related_name="responses")
     answer = models.TextField(default="")
     is_correct = models.BooleanField(default=False)
