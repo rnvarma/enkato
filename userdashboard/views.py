16050d0d1944e7b45c2e2490e3e7c437679339e9
Jared Moore <jmoore1@andrew.cmu.edu>
2016-07-26 17:04:03 -0400

    Combined user dashboard APIs

--- userdashboard/views.py
+++ userdashboard/views.py
@@ -1,17 +1,22 @@
 from django.db.models.query import Prefetch
 
 from backend.models import *
-from backend.serializers import StudentAnalyticsSerializer
+from backend.serializers import StudentAnalyticsSerializer, DashboardSeriesSerializer
 from backend.permissions import make_owner_permission
 
-from rest_framework import viewsets, filters, permissions
+from rest_framework import views, filters, permissions
+from rest_framework.response import Response
 
 
-class StudentAnalyticsViewset(viewsets.ModelViewSet):
-    serializer_class = StudentAnalyticsSerializer
+class StudentAnalyticsView(views.APIView):
     permission_classes = (permissions.IsAuthenticated,)
 
-    def get_queryset(self):
+    def get(self, request):
         user = self.request.user.customuser
         student_series_data = Prefetch('students_data', StudentSeriesData.objects.filter(user=user))
-        return user.student_series.prefetch_related(student_series_data)
+        return Response({
+            'subscribed_series': StudentAnalyticsSerializer(user.student_series.prefetch_related(student_series_data), many=True).data,
+            'created_series': DashboardSeriesSerializer(user.created_series.all(), many=True).data,
+            'all_unsubscribed_series': DashboardSeriesSerializer(Series.objects.filter(is_private=False).exclude(students=user), many=True).data,
+        })
+
