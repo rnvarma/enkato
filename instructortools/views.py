e4dbd5249ae44e0273a36f8071a8e2acd81e5c3f
Jared Moore <jmoore1@andrew.cmu.edu>
2016-07-27 16:25:05 -0400

    /1/instructoranalytics and /1/instructoranalytics/detailed is lit af

--- instructortools/views.py
+++ instructortools/views.py
@@ -2,18 +2,23 @@ from django.shortcuts import render
 from django.views.generic import View
 
 from backend.models import *
-from backend.serializers import InstructorSeriesSerializer
+from backend.serializers import InstructorSeriesSerializer, InstructorGeneralSeriesSerializer
 from backend.permissions import make_owner_permission
 
-from rest_framework import views, filters, permissions
+from rest_framework import viewsets, filters, permissions
 from rest_framework.response import Response
 
 
-class InstructorAnalyticsView(views.APIView):
+class InstructorAnalyticsView(viewsets.ViewSet):
     permission_classes = (permissions.IsAuthenticated,)  # TODO: needs owner authentication
 
-    def get(self, request):
-        user = self.request.user.customuser
+    def detailed(self, request):
+        user = request.user.customuser
         queryset = Series.objects.filter(creator=user)
         return Response(InstructorSeriesSerializer(queryset, many=True).data)
+
+    def general(self, request):
+        user = request.user.customuser
+        queryset = Series.objects.filter(creator=user)
+        return Response(InstructorGeneralSeriesSerializer(queryset, many=True).data)
 
