a90d71a2469c8c61ed14cbc0ae507e0140a9b4fa
Jared Moore <jmoore1@andrew.cmu.edu>
2016-08-10 17:35:19 -0400

    New Q&A style

--- assets/js/globals/QuestionAndAnswer/QuestionEditForm.jsx
+++ assets/js/globals/QuestionAndAnswer/QuestionEditForm.jsx
@@ -1,72 +1,77 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 
 import Button from 'react-bootstrap/lib/Button';
 
 import request from 'js/globals/HttpRequest';
 import QuestionForm from 'js/globals/QuestionAndAnswer/QuestionForm';
 
-class QuestionEditForm extends React.Component {
-  constructor() {
-    super();
+export default class QuestionEditForm extends Component {
+    static propTypes = {
+        question: PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            input: PropTypes.shape({
+                title: PropTypes.string.isRequired,
+                text: PropTypes.string.isRequired,
+                topic: PropTypes.string.isRequired,
+            }).isRequired,
+        }).isRequired,
+        topicList: PropTypes.array.isRequired,
+        pushQuestionEditText: PropTypes.func.isRequired,
+        replaceQuestion: PropTypes.func.isRequired,
+        toggleEdit: PropTypes.func.isRequired,
+        delete: PropTypes.func.isRequired,
+    }
 
-    this.onSubmit = this.onSubmit.bind(this);
-    this.onTopicChange = this.onTopicChange.bind(this);
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onTextChange = this.onTextChange.bind(this);
-    this.patch = this.patch.bind(this);
-  }
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.patch();
+    }
 
-  onSubmit(event) {
-    event.preventDefault();
-    this.patch();
-  }
+    onTopicChange = (e) => {
+        this.props.pushQuestionEditText(this.props.question.id, e.target.value, this.props.question.input.title, this.props.question.input.text);
+    }
 
-  onTopicChange(event) {
-    this.props.pushQuestionEditText(this.props.question.id, event.target.value, this.props.question.input.title, this.props.question.input.text);
-  }
+    onTitleChange = (e) => {
+        this.props.pushQuestionEditText(this.props.question.id, this.props.question.input.topic, e.target.value, this.props.question.input.text);
+    }
 
-  onTitleChange(event) {
-    this.props.pushQuestionEditText(this.props.question.id, this.props.question.input.topic, event.target.value, this.props.question.input.text);
-  }
+    onTextChange = (e) => {
+        this.props.pushQuestionEditText(this.props.question.id, this.props.question.input.topic, this.props.question.input.title, e.target.value);
+    }
 
-  onTextChange(event) {
-    this.props.pushQuestionEditText(this.props.question.id, this.props.question.input.topic, this.props.question.input.title, event.target.value);
-  }
-
-  patch() {
+    patch = () => {
     /* TODO: error handling on failing to patch */
-    const payload = {
-      title: this.props.question.input.title,
-      text: this.props.question.input.text,
-      topic_pk: (this.props.question.input.topic === 'General') ? null : this.props.question.input.topic,
-    };
-    request.patch(`/1/questions/${this.props.question.id}`, {
-      data: payload,
-      success: (data) => {
-        this.props.toggleEdit();
-        this.props.replaceQuestion(data.id, data);
-      }
-    })
-  }
+        const payload = {
+            title: this.props.question.input.title,
+            text: this.props.question.input.text,
+            topic_pk: (this.props.question.input.topic === 'General') ? null : this.props.question.input.topic,
+        };
+        request.patch(`/1/questions/${this.props.question.id}`, {
+            data: payload,
+            success: (data) => {
+                this.props.toggleEdit();
+                this.props.replaceQuestion(data.id, data);
+            },
+        });
+    }
 
-  render() {
-    return (
-      <div className="questionEditForm contentArea">
-        <QuestionForm
-          topicList={this.props.topicList}
-          onSubmit={this.onSubmit}
-          onTopicChange={this.onTopicChange}
-          onTitleChange={this.onTitleChange}
-          onTextChange={this.onTextChange}
-          titleValue={this.props.question.input.title}
-          textValue={this.props.question.input.text}
-          topicValue={this.props.question.input.topic}/>
-        <Button className="btn-primary" onClick={this.onSubmit}>Publish</Button>
-        <Button className="btn-secondary" onClick={this.props.toggleEdit}>Cancel</Button>
-        <Button className="btn-secondary" onClick={this.props.delete}>Delete</Button>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="questionEditForm contentArea">
+                <QuestionForm
+                    topicList={this.props.topicList}
+                    onSubmit={this.onSubmit}
+                    onTopicChange={this.onTopicChange}
+                    onTitleChange={this.onTitleChange}
+                    onTextChange={this.onTextChange}
+                    titleValue={this.props.question.input.title}
+                    textValue={this.props.question.input.text}
+                    topicValue={this.props.question.input.topic}
+                />
+                <Button className="btn-primary" onClick={this.onSubmit}>Publish</Button>
+                <Button className="btn-secondary" onClick={this.props.toggleEdit}>Cancel</Button>
+                <Button className="btn-secondary" onClick={this.props.delete}>Delete</Button>
+            </div>
+        );
+    }
 }
-
-export default QuestionEditForm;
