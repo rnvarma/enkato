85d0c5e6f20a9fd3e04c7f8d525b769deb5c8116
raitha <smilerkid@yahoo.com>
2016-08-12 13:22:01 -0400

    tabbed register modal

--- assets/js/globals/RegisterModal.jsx
+++ assets/js/globals/RegisterModal.jsx
@@ -7,7 +7,7 @@ import RegistrationForm from 'js/authentication/register/RegistrationForm';
 import LoginForm from 'js/authentication/login/LoginForm';
 
 import auth from 'auth';
-import request from 'js/globals/HttpRequest';   
+
 const RegisterModal = ({ registerModalOpen, closeRegisterModal, callbackFn, embed }) => {
     if (!auth.loggedIn()) {
         return (
@@ -20,16 +20,31 @@ const RegisterModal = ({ registerModalOpen, closeRegisterModal, callbackFn, embe
                         <Modal.Title>Login or Register to use this feature!</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        <LoginForm
-                            callbackFn={callbackFn}
-                            closeRegisterModal={closeRegisterModal}
-                            embed={embed}
-                        />
-                        <RegistrationForm
-                            callbackFn={callbackFn}
-                            closeRegisterModal={closeRegisterModal}
-                            embed={embed}
-                        />
+                        <ul className="nav nav-tabs" id="tabContent">
+                            <li className="active">
+                                <a href="#register" data-toggle="tab">Register</a>
+                            </li>
+                            <li>
+                                <a href="#log-in" data-toggle="tab">Log In</a>
+                            </li>
+                        </ul>
+
+                        <div className="tab-content">
+                            <div className="tab-pane active" id="register">
+                                <RegistrationForm
+                                    callbackFn={callbackFn}
+                                    closeRegisterModal={closeRegisterModal}
+                                    embed={embed}
+                                />
+                            </div>
+                            <div className="tab-pane" id="log-in">
+                                <LoginForm
+                                    callbackFn={callbackFn}
+                                    closeRegisterModal={closeRegisterModal}
+                                    embed={embed}
+                                />
+                            </div>
+                        </div>
                     </Modal.Body>
                 </Modal>
             </li>
@@ -56,6 +71,6 @@ RegisterModal.propTypes = {
     closeRegisterModal: PropTypes.func.isRequired,
     callbackFn: PropTypes.func.isRequired,
     embed: PropTypes.bool,
-}
+};
 
 export default RegisterModal;
