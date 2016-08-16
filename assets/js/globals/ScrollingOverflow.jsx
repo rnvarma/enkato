352051cd4654187fc0fd6a228ddd0ad3e980bc9a
rnvarma <rohan@prompthire.com>
2016-08-16 00:26:55 -0400

    added jared testimonial to jobs page

--- assets/js/globals/ScrollingOverflow.jsx
+++ assets/js/globals/ScrollingOverflow.jsx
@@ -3,49 +3,49 @@ import React from 'react';
 export default class TopicNode extends React.Component {
     constructor(props) {
         super(props);
-        this.getDimensions = this.getDimensions.bind(this)
-        this.onHoverOn = this.onHoverOn.bind(this)
-        this.onHoverOff = this.onHoverOff.bind(this)
+        this.getDimensions = this.getDimensions.bind(this);
+        this.onHoverOn = this.onHoverOn.bind(this);
+        this.onHoverOff = this.onHoverOff.bind(this);
 
         this.state = {
             overflow: false,
             totalWidth: 0,
             parentWidth: 0,
-        }
+        };
     }
 
     getDimensions() {
-        var elem = $(this.refs.dom)
-        var parent = elem.parent();
-        var parentWidth = parent.width()
-        parent.css("width", "initial")
-        var totalWidth = elem.width() + 15
+        let elem = $(this.refs.dom);
+        let parent = elem.parent();
+        let parentWidth = parent.width();
+        parent.css('width', 'initial');
+        let totalWidth = elem.width() + 15;
         this.setState({
             overflow: totalWidth > parentWidth,
-            totalWidth: totalWidth,
-            parentWidth: parentWidth
-        })
-        parent.css("width", this.props.elementSize)
+            totalWidth,
+            parentWidth,
+        });
+        parent.css('width', this.props.elementSize);
     }
 
     componentDidMount() {
-        this.getDimensions()
-        $(window).on('resize', this.getDimensions)
+        this.getDimensions();
+        $(window).on('resize', this.getDimensions);
     }
 
     componentWillUnmount() {
-        $(window).off('resize')
+        $(window).off('resize');
     }
 
     onHoverOn() {
         if (this.state.overflow) {
-            $(this.refs.dom).css("margin-left", this.state.parentWidth - this.state.totalWidth)
+            $(this.refs.dom).css('margin-left', this.state.parentWidth - this.state.totalWidth);
         }
     }
 
     onHoverOff() {
         if (this.state.overflow) {
-            $(this.refs.dom).css("margin-left", 0);
+            $(this.refs.dom).css('margin-left', 0);
         }
     }
 
