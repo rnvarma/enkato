5970bb6d66451fd0d629d79e061316e327ecece1
rnvarma <rohan@prompthire.com>
2016-08-16 00:40:37 -0400

    updated sll verification

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 from home.views import *
 
 urlpatterns = [
-    url(r'^.well-known/acme-challenge/PRmzb9_uMgi5cpDfBDc4ZRBO-BDOJUVI4VgfgWonNzA$', SSLPage.as_view()),
+    url(r'^.well-known/acme-challenge/LlVITAf22WhAg7BOReBaMjWVL1vAe5yxm_I3w4zqbas$', SSLPage.as_view()),
     url(r'^jobs$', JobsPage.as_view()),
     url(r'^', HomePage.as_view()),
     url(r'^interesteduser$', csrf_exempt(InterestedUserRegistration.as_view()))
